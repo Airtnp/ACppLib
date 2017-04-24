212cabc09566b9cd5d3f7308bd31a9e62731d180
Airtnp <xiaolr2003@foxmail.com>
2017-04-24 12:59:41 +0800

    fengkuang moyu

---
+++ src/sn_JSON.hpp
@@ -0,0 +1,13 @@
+#ifndef SN_JSON_H
+#define SN_JSON_H
+
+#include "sn_CommonHeader.h"
+
+namespace sn_JSON {
+	namespace container {
+
+	}
+}
+
+
+#endif
---
+++ src/sn_SM.hpp
@@ -0,0 +1,16 @@
+#ifndef SN_SM_H
+#define SN_SM_H
+
+#include "sn_CommonHeader.h"
+
+// ref: https://github.com/eglimi/cppfsm
+// ref: https://github.com/digint/tinyfsm
+// ref: https://github.com/boost-experimental/sml
+namespace sn_SM {
+
+
+}
+
+
+
+#endif
