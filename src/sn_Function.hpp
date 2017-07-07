9618c636064ebc154f1f3d2e752a4c2da3c3c0f3
Airtnp <xiaolr2003@foxmail.com>
2017-07-07 18:27:03 +0800

    Add failable

--- src/sn_Function.hpp
+++ src/sn_Function.hpp
@@ -1255,6 +1255,48 @@ namespace sn_Function {
 
 	}
 
+#ifdef SN_ENABLE_CPP17_EXPERIMENTAL
+	namespace failable {
+		
+		template <typename R, typename ... P>
+		auto make_failable(R (*f)(P ... ps)) {
+			return [f](std::optional<P> ... xs) -> std::optional<R> {
+				if ((xs && ...)) {
+					return {f(*(xs)...)};
+				} else {
+					return {};
+				}
+			};
+		}
+
+		template <typename F>
+		auto make_failable(F&& f) {
+			return [f = std::forward<F>(f)](auto&&... xs)
+				-> std::optional<decltype(std::forward<F>(f).get()(*(std::forward<decltype(xs)>(xs))...))> {
+				if ((xs && ...)) {
+					return std::forward<F>(f).get()(*(std::forward<decltype(xs)>(xs)...));
+				} else {
+					return {};
+				}
+			}
+		}
+
+		// join
+		template <typename R, typename ... P>
+		auto make_failable(std::optional<R> (*f)(P ... ps)) {
+			return [f](std::optional<P> ... xs) -> std::optional<R> {
+				if ((xs && ...)) {
+					return f(*(xs)...);
+				} else {
+					return {};
+				}
+			};
+		}
+	}
+
+
+#endif
+
 
 	using function::make_func;
 	using currying::make_curry;
