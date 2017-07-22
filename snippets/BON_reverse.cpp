ee0c23618bc9bf96585e8b2a21ce5a358895167e
Airtnp <xiaolr2003@foxmail.com>
2017-07-22 19:39:46 +0800

    Add reverse_copyable

---
+++ snippets/BON_reverse.cpp
@@ -0,0 +1,92 @@
+// ref: https://gcc.godbolt.org/#z:OYLghAFBqd5QCxAYwPYBMCmBRdBLAF1QCcAaPECAKxAEZSBnVAV2OUxAHIBSAJgGY8AMwDUWIXgB2mdBAD6AWTkBJABoAOAGwBKEdwDsAYQOGxmCdNmK5qzQBZt3AAwBBTJOYBbEcoDKLuRcANRdlABkXACFw5QAVAE1nFwNIpJF0kTkVf0CQ8Kiw7BstDO5%2BABERJ1I0jKy/AODQiMjCuV9fbF50ssr6WvT6nKb81qKO7Dtu3pFeGtcMzOzGvJa24NVSipF%2BeZdFoZXmgqLsADkAMV9YgCUAcT1tuz2D5dzjsdzVae2AViSDOUyqlXElMAAPAiYYiSPS8XjGeEiCFQmEiAA2qEkwCWeAYAEM5O58QAjdEyYEA0ECKTIdHMLCPQxSAjEKQAOgQZWwAJpkjpDMwTMwnk8LLZkk53N5gn59MZZUM4NF4o5XP4PNBrihngADuj8VDFXT8QwGJlInh0MoCNKtQBOBgEQ14ZAiKToqRCgBuqCtmRumG90IYmDkzrw6PklutBEyFzwxCdpAtVptmTCpoIugICGIqAA7hBHA6UlT7UISCIIMD44mnSIwGAZnJMw2%2BJoO3Wk3GmzMALT91tZ2t8VK8SJyBM9kvJJz25wL/QgucL%2BdoSQN/HMIiZWIi3WPSoAKin9dt/BXi8Xp%2Bn7e2p7bF6v85vw/vlTk%2B71lNLr/0QKlgBVIuE6Lpuh6XoiAweAAF5hnGciRAAnlCcjktiuYQL6/rHt2yYiDh6AiHh77ZiIub5kWs7Xsu5bEJgBCsLCEBgQQrpyMgI78IYzCbngwCWCIyAIPixAkdy8hPro/bQc67HIJx3G8fxgkyMJoniceklnjOjiXuWgIgWxrrupInrSIRfrEYE6Devi/JhiSqGYAw2HWRJvCaHuYnAIxKa6qy%2BBCEI4aZAA8iFobkZRhbFoZdEOl%2BvmMUecngUpTqKkREkaqx8kcVxWU8XxMFqcRIlibl2DyLEKXkWOEVRYx%2BkvkugFzm4kLQrCfAIn1eiJZ1WRYHSDC6pgyAQJIqD4p6pq6DlWTIKN6JLE66ByAxwZJghbLengc3oshnGoLqyGkuSci0O5uH4QQKY5aR0kiDNELsIFCVtcI1ZIS56HuMAWG6QRZG6NK2z2HCnZeZkJKEFCTo1l5WR4oSxJkjIKaHO8ozrEE3zaDRr7/m1i4bg2WSeLwvyaHgAZBiGYaVcQ6EOa5cj4t64JpXIoo05ocjRUSup4OoNZ/mu9rVCI9CzCmuwiM8Ii/CmmgpvoKbqCm9oprQMu0HLtBzLLiu0MrtCq4ZJPS3rKYm4ryuqyI6siJrIjayIuuywbRsm7QZsW78rXW/aT2ZL4RC6pzcYtnez6h7Z9mOXIzmI/IkdnTH2MoWhGFAwg8jx9jL0Q9gLtMoqKsh5L6CoKHtFkzbScOewqcuW5ZEpv2/C8DXq7XvOAD0Q%2BERNxCeAAjhRqAYrNxHkkIcYOcRbLAAgD0iCvFHEA542oKGFEIEKBrSAwDfrlilN8wL9OtuYsfbHz1O03I4%2BeDumB2OC9gi3g9jyH5q/TE%2BJ0DMCFngAW%2BUMpFQvIYKmt9qpF3PETDW6h%2B6DylhTRCN9aZ3xuAJDevMgGC3fp/b%2Bv9MCiwAc/AW6F57gJglAkyilYGKgQXgpBYNtBoIwTbRcI8d5711AfIUR0j5ChZuaKQEiMTs3PpLcmV8cEvzphmB%2BW1Ga7WIi2EhQsEDMBCldKhYspIP2xoGHaoZOKaTZmfTm3M%2BFSyUZuFRiC5AEPXgQTRVj1K6NUfowxQhjGi3Fh4whm8PFaOsSzOxHMubgicZg2hr8nQkEwIwyBtNoEKUynAjhajtJ5RBtmCxESfFM3QEk5uei0kMUycwgqrDlIFPpkUmqYMS4aO2pU6pUsW4pzTq5ZBPYFZ91/APfhwFJYFgQJGIUIyGx9ifpnaOho%2BlGS1KuH60Z/r52BsXDMWZwYahmL3aGXZU4I1cgQZGgs5BoyJJIS6WMlgNFxmscYnQphEwbkNTB2ClieGNuofB0Tma2LNEKfxQtGJ/3Fgbe2CsUxOzVhrLWOs9a%2Bz1v7QOetg4TMweHdoUcY683joS5uLg7Ktych3DOpLDQ5z2YDA554S7HMeDyDUKtK48SVn0uuF8lxN36dS5ObchmdyfN3WgOhKXOMvq4oFIK75hAfsQzw9DQGZJBTkwqLS%2Baqq4fHX5BlFFKuvsC3goKAwRM1dqsBEC9UsLyewo1Nq2k6Wkhsy1KjjX3yXhU7RDqGAGKMWGExYT1VLwseCmxYkhahlaiIQRHROj8GFYCqmAbPEb2DaGHRT9RSBIjfC%2BQebImWKZgm1mULfX2mfkLIg9TnU2v1c04q8CPW2vaYs0pdqvEFpkA2ptdSMltvFq6thPEc2eq4dJLpQaenaNHeK2l7d04lL1vK81kznHTP3bM%2BZv146NmbCsxl2YFW0Q6uWKJvjwz4kjB2t1JVVJCRZialBKZp3KVKgJT9mkF3HKSYewR3BfiRBEOyWDehfh3tBABVNeEUQ9ThP1JEx4h4gT4PwdwwUUNmAsDIeQSg1BaHBkYEwxGvRWCULYBwJEcOuE4Dw9EXBficFIJILgThuOoC4Iicc0GmCsHYHCfgtBuMED42xnhABrEAvxqgcc4HYbjvHOD8dIIJzg3GGAgGqLJ7TbHSBwFgEgNAep5lkAoBAazotyTEBQOiWgjb7CkAkOiVEhn5BZCcJQHhJI5OkGs54dwBBwrmWQqF/ADFkDsWDIZ0z5BJBQjUzpqgk9mDQli5wfg7JeDsmk6QfZSB6C6m2uFEkVBJqyf83IQLrE4KwXJJbJwThNCaHnPwfQNN9CUD4uIOjlByR%2BUkOgeAqtqicGk0TVB5W6CkCq0GGrdXEuUCyE1ygVAACKuXiDIV7vrJwvxaD6HUNLOwdh7Tueu/rOwtBKD4HGgafLZmYKwTa5gDrXWev2j6wNobk3zCjYgONgj03SCzfmwtsrrKKvw7OuxK%2BXAh7YAABIMDdEPAA6nYVN4VeAGZYGwDgtB2Occ06FvT4ItD9ihoYMIst7SzCcBdkQNxYh4%2BrLgQgVY8Ny0MKgGzznJOU5k3J1BSmVNla4BpnjtOuAGaM6QEz/HFtcBJ0r1LempemdQVYvAWIQB2CAA%3D
+#if defined(_M_IX86) || defined(_M_X64)
+enum ISA_AVAILABILITY
+{
+    __ISA_AVAILABLE_X86   = 0,
+    __ISA_AVAILABLE_SSE2  = 1,
+    __ISA_AVAILABLE_SSE42 = 2,
+    __ISA_AVAILABLE_AVX   = 3,
+    __ISA_AVAILABLE_ENFSTRG = 4,
+    __ISA_AVAILABLE_AVX2 = 5
+};
+
+extern "C" extern long __isa_enabled;
+
+#include <intrin.h>
+#include <emmintrin.h>
+#include <xmmintrin.h>
+
+template<class _BidIt>
+	static inline void _Reverse_tail(_BidIt _First, _BidIt _Last) throw()
+	{
+	for (; _First != _Last && _First != --_Last; ++_First)
+		{
+		const auto _Temp = *_First;
+		*_First = *_Last;
+		*_Last = _Temp;
+		}
+	}
+
+static inline size_t _Byte_length(void * _First, void * _Last) throw()
+	{
+	return (static_cast<unsigned char *>(_Last) - static_cast<unsigned char *>(_First));
+	}
+
+static inline void _Advance_bytes(void *& _Target, ptrdiff_t _Offset) throw()
+	{
+	_Target = static_cast<void *>(static_cast<unsigned char *>(_Target) + _Offset);
+	}
+
+extern "C" {
+
+__declspec(noalias) void __cdecl __std_reverse_trivially_copyable_1(void * _First, void * _Last) noexcept
+	{
+	if (_Byte_length(_First, _Last) >= 64 && _bittest(&__isa_enabled, __ISA_AVAILABLE_AVX2))
+		{
+		const __m256i _Reverse_char_lanes_avx = _mm256_set_epi8(
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+		void * _Stop_at = _First;
+		_Advance_bytes(_Stop_at, _Byte_length(_First, _Last) >> 6 << 5);
+		do
+			{
+			_Advance_bytes(_Last, -32);
+			// vpermq to load left and right, and transpose the lanes
+			const __m256i _Left = _mm256_permute4x64_epi64(_mm256_loadu_si256(static_cast<__m256i *>(_First)), 78);
+			const __m256i _Right = _mm256_permute4x64_epi64(_mm256_loadu_si256(static_cast<__m256i *>(_Last)), 78);
+			// transpose all the chars in the lanes
+			const __m256i _Left_reversed = _mm256_shuffle_epi8(_Left, _Reverse_char_lanes_avx);
+			const __m256i _Right_reversed = _mm256_shuffle_epi8(_Right, _Reverse_char_lanes_avx);
+			_mm256_storeu_si256(static_cast<__m256i *>(_First), _Right_reversed);
+			_mm256_storeu_si256(static_cast<__m256i *>(_Last), _Left_reversed);
+			_Advance_bytes(_First, 32);
+			}
+		while (_First != _Stop_at);
+		}
+
+	if (_Byte_length(_First, _Last) >= 32 && _bittest(&__isa_enabled, __ISA_AVAILABLE_SSE42))
+		{
+		const __m128i _Reverse_char_sse = _mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+		void * _Stop_at = _First;
+		_Advance_bytes(_Stop_at, _Byte_length(_First, _Last) >> 5 << 4);
+		do
+			{
+			_Advance_bytes(_Last, -16);
+			const __m128i _Left = _mm_loadu_si128(static_cast<__m128i *>(_First));
+			const __m128i _Right = _mm_loadu_si128(static_cast<__m128i *>(_Last));
+			const __m128i _Left_reversed = _mm_shuffle_epi8(_Left, _Reverse_char_sse); // SSSE3
+			const __m128i _Right_reversed = _mm_shuffle_epi8(_Right, _Reverse_char_sse);
+			_mm_storeu_si128(static_cast<__m128i *>(_First), _Right_reversed);
+			_mm_storeu_si128(static_cast<__m128i *>(_Last), _Left_reversed);
+			_Advance_bytes(_First, 16);
+			}
+		while (_First != _Stop_at);
+		}
+
+	_Reverse_tail(static_cast<unsigned char *>(_First), static_cast<unsigned char *>(_Last));
+	}
+// [ ... ]
+
+} /* extern "C" */
+
+#endif /* defined(_M_IX86) || defined(_M_X64) */
