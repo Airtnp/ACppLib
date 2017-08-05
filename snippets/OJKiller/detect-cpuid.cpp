251d3865817651f62c89d5b79714ea1402db8a50
Airtnp <xiaolr2003@foxmail.com>
2017-08-05 13:52:58 +0800

    Add detect cpuid

---
+++ snippets/OJKiller/detect-cpuid.cpp
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <iostream>
+#include <cpuid.h>
+static void cpuid(uint32_t func, uint32_t sub, uint32_t data[4]) {
+    __cpuid_count(func, sub, data[0], data[1], data[2], data[3]);
+}
+int main() {
+    uint32_t data[4];
+    char str[48];
+    for(int i = 0; i < 3; ++i) {
+        cpuid(0x80000002 + i, 0, data);
+        for(int j = 0; j < 4; ++j)
+            reinterpret_cast<uint32_t*>(str)[i * 4 + j] = data[j];
+    }
+    std::cout << str;
+}
