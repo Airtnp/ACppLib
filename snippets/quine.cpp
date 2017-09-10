72567f8b4ee93bf3b6bdec99c0daccf6c3eedb2c
Airtnp <xiaolr2003@foxmail.com>
2017-09-10 12:40:48 -0400

    Remove cpp snippets

--- snippets/quine.cpp
+++
@@ -1,3 +0,0 @@
-#include <cstdio>
-char s[] = "#include <cstdio>%cchar s[] = %c%s%c%c%cint main(){std::printf(s,10, 34, s, 34, 59, 10);}";
-int main(){std::printf(s,10, 34, s, 34, 59, 10);}
--- snippets/substr.c
+++
@@ -1,4 +0,0 @@
-void del_substr(char *str, const char *substr) {
-  for (int i = 0, j = 0; *str = *(str+i); str += !!*(substr+j), i += !*(substr+j) * j)
-    for (j = 0; *(str+i+j) == *(substr+j) && *(substr+j); j++);
-}
